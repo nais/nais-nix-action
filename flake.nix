555607d74265c8125b40a5cf93cea215ea25f645
carl hedgren <carl.hedgren@nav.no>
2024-12-05 13:17:34 +0100

    we dont call w. system

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Helper functions for NAIS flake apps";
   outputs = { self, nixpkgs }: {
     lib = {
-      analyzeNaisAppsToFile = { registry, project, team, system }:
+      analyzeNaisAppsToFile = { registry, project, team }:
         apps:
         let
           pkgs = nixpkgs.legacyPackages.${system};
